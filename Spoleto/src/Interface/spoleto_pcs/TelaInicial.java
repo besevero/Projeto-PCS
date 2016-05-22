b1e464dd4b3e6446e58cab45164f9205840efcb7
besevero <besevero@gmail.com>
2016-05-21 23:48:07 -0300

    Ajustes
    Ajustes menores nas Classes para o MVC

--- Spoleto/src/Interface/spoleto_pcs/TelaInicial.java
+++ Spoleto/src/Interface/spoleto_pcs/TelaInicial.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package spoleto_pcs;
+package Interface.spoleto_pcs;
 
 import javax.swing.ImageIcon;
 
