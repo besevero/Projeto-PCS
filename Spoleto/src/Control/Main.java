d479138d9d486b6afa3dffbe6392d04a4c9aeb8e
besevero <besevero@gmail.com>
2016-05-22 00:27:53 -0300

    Ajustes menores

---
+++ Spoleto/src/Control/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Interface.spoleto_pcs.TelaInicial;
+
+/**
+ *
+ * @author Bernardo
+ */
+public class Main {
+     public static void main(String[] args) {
+    
+     }
+    
+}
