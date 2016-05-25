61f25c8e4913851de9ef81596ab3da50d73eab74
besevero <besevero@gmail.com>
2016-05-24 21:04:28 -0300

    alterações

---
+++ Spoleto_Interface/src/Model/Pedido.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PPGI
+ */
+public class Pedido {
+      private ArrayList<Produto> pratos = new ArrayList();
+      private int senha;
+
+    public Pedido(int senha) {
+        this.senha = senha;
+    }
+
+   
+        
+    void incluirProdPed(Produto p){
+            getPratos().add(p);
+    }
+        
+    ArrayList<Produto> mostrarPedido(){
+        return getPratos();
+    }
+
+    /**
+     * @return the pratos
+     */
+    public ArrayList<Produto> getPratos() {
+        return pratos;
+    }
+
+    /**
+     * @param pratos the pratos to set
+     */
+    public void setPratos(ArrayList<Produto> pratos) {
+        this.pratos = pratos;
+    }
+
+    /**
+     * @return the senha
+     */
+    public int getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+        
+}
